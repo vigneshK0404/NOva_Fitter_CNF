878fdfc79e59eac54762269aea4414c0a43863b7
Vignesh <vigneshwar.karthik04@gmail.com>
2026-06-05 09:34:04 -0400

    fixing readCNFROOT streamlining for multiple files, cleaning

--- heimdallScripts/readCNFROOT.py
+++ heimdallScripts/readCNFROOT.py
@@ -24,18 +24,22 @@ def plotBinnedData(data : np.array, path : str):
     plt.savefig(path+".png")
 
 
-def standardize(training_path : str):
+def calculate_std(base_path : str):
 
-    files = sorted(glob(training_path))
+    files = sorted(glob(f"{base_path}training/"))
     theta_sum = 0
     num_samples = 0
     theta_vars = 0
     populus = len(files)
+    num_data = 0
+    meanSlices = []
+    stdSlices = []
+    binList = [22,22,22,22,14,14,13,13,6]
+
 
     for file_name in files:
         x = np.load(file_name)
         theta = x["params"]
-        data = x["data"]
         theta_unique = theta[::repeatSample,:]
 
         theta_sum += theta_unique.sum(axis=0)
@@ -43,66 +47,51 @@ def standardize(training_path : str):
         theta_vars += np.var(theta_unique,axis = 0,ddof=1) * (len(theta_unique)-1)
         
         #print(theta[np.abs(theta) > 3.5])
-
-        binList = [22,22,22,22,14,14,13,13,6]
+        data = x["data"]
         data_AT = 2 * np.sqrt(data + 3/8)
 
-        slices = []
-        index = 0
+        index = 0                
+        sumSlices = [0,0,0,0,0,0,0,0,0]
+        varSlices = [0,0,0,0,0,0,0,0,0]
+        numSlices = [0,0,0,0,0,0,0,0,0]
+        num_data += len(data_AT)
 
-        meanSlices = []
-        stdSlices = []
-        
+        idx = 0
+
         for i in binList:
             slicedData = data_AT[:,index:i+index]
             index += i
-            slicedMean = np.mean(slicedData , axis = 0)
-            slicedStd = np.std(slicedData, axis=0, ddof=0)
-            meanSlices.append(slicedMean)
-            stdSlices.append(slicedStd)
-            slices.append((slicedData - slicedMean) / (slicedStd + EPSILON))
-
-
-        data_AT = np.concatenate(slices,axis=1)
+            sumSlices[idx] += np.sum(slicedData,axis=0)
+            varSlices[idx] += np.var(slicedData,axis-0,ddof=1) * (len(data_AT) - 1)
+            numSlices[idx] += len(data_AT)
+            idx += 1
     
     theta_mean = theta_sum/num_samples
     theta_std = np.sqrt(theta_vars/(num_samples-populus))
-    #save theta_mean, theta_std, 
-    with open(f"{base_path}{file_name}_slice_stats.pkl", "wb") as f:
+
+    for i in range(len(sumSlices)):
+        meanSlices.append(sumSlices[i]/num_data)
+        stdSlices.append(np.sqrt(varSlices[i]/(numSlices[i] - populus)))
+    
+    with open(f"{base_path}processed/slice_stats.pkl", "wb") as f:
         pickle.dump({
             "meanSlices": meanSlices,
             "stdSlices": stdSlices
         }, f)
 
+    np.save(f"{base_path}processed/theta_mean",theta_mean)
+    np.save(f"{base_path}processed/theta_std",theta_std)
     
-    return theta , theta_mean, theta_std, data_AT , meanSlices, stdSlices
+    return
 
 
-def applyStd(theta : np.array, theta_mean : np.array, 
-             theta_std : np.array, data : np.array, 
-             meanSlices : list, stdSlices : list):
+def applyStd(base_path : str, apply_site : str): #apply_site is either training or testing
 
-    theta = (theta - theta_mean)/(theta_std + EPSILON)
+    files = sorted(glob(f"{base_path}{apply_site}/"))
 
+    theta_mean = np.load(f"{base_path}processed/theta_mean.npy")
+    theta_std = np.load(f"{base_path}processed/theta_std.npy")
     binList = [22,22,22,22,14,14,13,13,6]
-    data_AT = 2 * np.sqrt(data + 3/8)
-
-    slices = []
-    index = 0
-
-    idx = 0 
-    for i in binList:
-        slicedData = data_AT[:,index:i+index]
-        index += i      
-        slices.append((slicedData - meanSlices[idx]) / (stdSlices[idx] + EPSILON))
-        idx += 1
-
-    data_AT = np.concatenate(slices,axis=1)
-
-    return theta, data_AT
-
-
-def applyStdtoData(data : np.array):
 
     with open("slice_stats.pkl", "rb") as f:
         data = pickle.load(f)
@@ -111,29 +100,30 @@ def applyStdtoData(data : np.array):
     stdSlices = data["stdSlices"]
 
 
-    if data.ndim == 1:
-        data = np.expand_dims(data,axis=0)
-
-    binList = [22,22,22,22,14,14,13,13,6]
-    data_AT = 2 * np.sqrt(data + 3/8)
-
-    slices = []
-    index = 0
-
-    idx = 0 
-    for i in binList:
-        slicedData = data_AT[:,index:i+index]
-        index += i      
-        slices.append((slicedData - meanSlices[idx]) / (stdSlices[idx] + EPSILON))
-        idx += 1
-
-    data_AT = np.concatenate(slices,axis=1)
-
-    return data_AT
-
+    for file_name in files:
+        x = np.load(file_name)
+        theta = x["params"]
+        theta = (theta - theta_mean)/(theta_std + EPSILON)
     
+        data = x["data"]
+        data_AT = 2 * np.sqrt(data + 3/8)
 
+        slices = []
+        index = 0
+        idx = 0 
 
+        for i in binList:
+            slicedData = data_AT[:,index:i+index]
+            index += i      
+            slices.append((slicedData - meanSlices[idx]) / (stdSlices[idx] + EPSILON))
+            idx += 1
+
+        data_AT = np.concatenate(slices,axis=1)
+
+        np.savez(f"{base_path}processed/{file_name}", data = data_AT, params = theta)
+
+    return
+ 
 
 def getSterileData(base_path : str):
 
