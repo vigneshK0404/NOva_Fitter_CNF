fad4a75273917db3a8a8afddddac8bf9b3048c25
Vignesh <vigneshwar.karthik04@gmail.com>
2026-07-12 01:50:34 -0400

    added DBSCAN too many outliers need to investigate

--- heimdallScripts/diagnose.py
+++ heimdallScripts/diagnose.py
@@ -4,6 +4,8 @@ from glob import glob
 import consts
 import random
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
 
 """
 Find ideal eps and min_freq, plotting or ML technique may be required
@@ -35,12 +37,12 @@ def estimate_params(freqs : list, num_random_files : int, num_random_rows : int)
     theta_array = np.vstack(theta_list)
     query_array = np.vstack(query_list)
 
-    print(theta_array.shape)
-    print(query_array.shape)
+    print(f"theta_array : {theta_array.shape}")
+    print(f"query_array : {query_array.shape}")
 
     tree = cKDTree(theta_array)
 
-    for min_freq in freqs:
+    for min_freq in tqdm(freqs):
         dd , _ = tree.query(query_array, k=min_freq+1)
         R_array = dd[:,-1]
         R_stds.append(np.std(R_array, ddof=0))
@@ -49,8 +51,21 @@ def estimate_params(freqs : list, num_random_files : int, num_random_rows : int)
     
     R_stds = np.array(R_stds)
     R_means = np.array(R_means)
+    R_score = R_stds / R_means
 
-    smallest_idx = np.argmin(R_stds / R_means)
+    smallest_idx = np.argmin(R_score)
+
+    plt.plot(freqs,R_score)
+    plt.savefig("plots/R_score.png")
+    plt.clf()
+
+    plt.plot(freqs,R_means)
+    plt.savefig("plots/R_means.png")
+    plt.clf()
+
+    plt.plot(freqs,R_stds)
+    plt.savefig("plots/R_stds.png")
+    plt.clf()
 
     return R_means[smallest_idx] , freqs[smallest_idx]
 
@@ -58,37 +73,63 @@ def estimate_params(freqs : list, num_random_files : int, num_random_rows : int)
 
 
 
-def find_holes_in_data():
+def find_holes_in_data(thetaMean : np.array, thetaStd : np.array):
+    
+    eps = 0.4488
+    min_freq = 72
+
+    theta_paths = sorted(glob(consts.theta_path))
+    outliers = []
+
+    theta_list = []
+
+    for idx in tqdm(range(len(theta_paths))):
+        path = theta_paths[idx]
+        theta_list.append(np.load(path)[::consts.repeatSize])
+
+        if idx % 5 == 0 and idx != 0 :
+            theta_array = np.concatenate(theta_list , axis=0)            
+            db = DBSCAN(eps=eps, min_samples=min_freq).fit(theta_array)
+            mask = db.labels_ == -1
+            outliers.append(theta_array[mask])
+
+            theta_list = []
+
+    if theta_list:
+        theta_array = np.concatenate(theta_list , axis=0)            
+        db = DBSCAN(eps=eps, min_samples=min_freq).fit(theta_array)
+        mask = db.labels_ == -1
+        outliers.append(theta_array[mask])
         
-    undersamples = []
 
-    for path in tqdm(theta_paths[:5]):
-        theta_array = np.load(path)
-        arr_list = np.array_split(theta_array,30)
-        tree = KDTree(theta_array) 
-       
 
-        for arr in arr_list:
-            query_tree = KDTree(arr)
-            n_idx = query_tree.query_ball_tree(tree,R)
-            n_lens = np.fromiter((len(indices) for indices in n_idx), dtype=np.int64, count=len(arr))
-            query_idx = np.flatnonzero(n_lens < 1001)
-            undersamples.append(arr[query_idx])
-            #n_lens = tree.query_ball_point(arr, R, return_sorted=False, return_length=True)
-            #query_idx = np.where(n_lens < 1001)[0]
-            print(len(query_idx))
-            #undersamples.append(arr[query_idx])               
+    outliers = np.concatenate(outliers, axis=0)
+    print(f"outliers : {outliers.shape}")
+
+    db_outliers = DBSCAN(eps=eps, min_samples=min_freq).fit(outliers)
+    mask = db_outliers.labels_ == -1
+
+    outliers_final = outliers[mask]
     
-    
-    return
+
+    outliers_final *= (thetaStd + consts.EPSILON)
+    outliers_final += thetaMean
+
+    return outliers_final
 
 
 
 if __name__ == "__main__":
-    freqs = [10,15,20,25,30,35,40,45,50]
-    num_random_files = 50
-    num_random_rows = 200
+    #freqs = list(range(10,150))
+    #num_random_files = 50
+    #num_random_rows = 200
     
-    print(estimate_params(freqs,num_random_files,num_random_rows))
+    #print(estimate_params(freqs,num_random_files,num_random_rows))
 
-    #find_holes_in_data()
+    thetaMean = np.load(consts.theta_mean_path)
+    thetaStd = np.load(consts.theta_std_path) 
+
+    outliers = find_holes_in_data(thetaMean,thetaStd)
+
+    print(outliers)
+    print(outliers.shape)
